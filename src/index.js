0481450414621a849d2c86d0ef31861634d86223
BeauBryanDev <bryan.beau.186@gmail.com>
2025-11-05 18:00:00 -0500

    edition :  set random changes for testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Template from '@templates/Template.js';
 import '@styles/styles.css';
-
+// edit root file to asset// edit root file to assetss
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
